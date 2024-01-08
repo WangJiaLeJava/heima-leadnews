332fca45e9b44f413fd639f8f8b9a761d9649866
WangJiaLeJava <1491710707@qq.com>
2024-01-08 17:31:16 +0800

    1.8 heima-gateway

--- heima-leadnews-model/src/main/java/com/heima/user/model/user/dtos/LoginDto.java
+++ heima-leadnews-model/src/main/java/com/heima/user/model/user/dtos/LoginDto.java
@@ -1,13 +1,19 @@
 package com.heima.user.model.user.dtos;
 
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 public class LoginDto {
 
+
+    @ApiModelProperty(value = "手机号",required = true)
     private String phone;
 
+
+    @ApiModelProperty(value = "密码",required = true)
     private String password;
 
 
--- heima-leadnews-service/heima-leadnews-user/src/main/java/com/heima/user/controller/v1/ApUserLoginController.java
+++ heima-leadnews-service/heima-leadnews-user/src/main/java/com/heima/user/controller/v1/ApUserLoginController.java
@@ -3,6 +3,8 @@ package com.heima.user.controller.v1;
 import com.heima.user.model.common.dtos.ResponseResult;
 import com.heima.user.model.user.dtos.LoginDto;
 import com.heima.user.service.ApUserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,10 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/login")
+@Api(value = "app端用户登录",tags = "app端用户登录")
 public class ApUserLoginController {
     @Autowired
     private ApUserService apUserService;
     @PostMapping("/login auth")
+    @ApiOperation("用户登录")
     public ResponseResult login(@RequestBody LoginDto dto){
 
          return apUserService.login(dto);
